32b6b974df711f8a3244dd5d1860a0c23daa2f01
seoyeunglee <syoo0183oo@gmail.com>
2024-12-09 01:14:08 +0900

    Knitly Ever After

--- snowProject/src/main/java/snowProject/controller/ItemController.java
+++ snowProject/src/main/java/snowProject/controller/ItemController.java
@@ -1,12 +1,17 @@
 package snowProject.controller;
 
+import java.io.PrintWriter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import snowProject.command.CartCommand;
+import snowProject.service.item.CartInsertService;
 import snowProject.service.item.CartListService;
 import snowProject.service.item.GoodsCartDelsService;
 import snowProject.service.item.GoodsWishListService;
@@ -20,8 +25,8 @@ public class ItemController {
 	CartListService cartListService;
 	@Autowired
 	GoodsCartDelsService goodsCartDelsService;
-	// @Autowired
-	// CartInsertService cartInsertService;	
+	@Autowired
+	CartInsertService cartInsertService;	
 	
 	@GetMapping("wishList")
 	public String wishList(HttpSession session, Model model) {
@@ -38,5 +43,29 @@ public class ItemController {
 		goodsCartDelsService.execute(goodsNums, session);
 		return "redirect:cartList";
 	}
+	
+	@GetMapping("buyItem")
+	public String buyItem(CartCommand cartCommand, HttpSession session
+			, HttpServletResponse response) {
+		String result = cartInsertService.execute(cartCommand, session);
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out;
+		if(result == "000") {
+			return "redirect:/";
+		}else if(result == "900") {
+			String str = "<script>"
+					   + "alert('관리자는 구매할 수 없습니다.');"
+					   + "location.href='/corner/detailView/"+cartCommand.getGoodsNum()+"';"
+					   +"</script>";
+			try {
+			out = response.getWriter();
+			out.print(str);
+			out.close();
+			}catch(Exception e) {}
+		}
+		return "redirect:/purchase/goodsBuy?nums="+cartCommand.getGoodsNum();
+	}
+	
+	
 	
 }
